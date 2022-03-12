4273066c30b5d58d73b88b4704a846e9e59ef25b
jacobharmon43 <jacobharmon43@gmail.com>
2022-03-12 13:07:55 -0600

    slam

--- Scripts/SlamController.cs
+++ Scripts/SlamController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SlamController : MonoBehaviour
 {
-    [SerializeField] private float damageTimer = 0.5f;
+    [SerializeField] private float damageTimer = 0.1f;
 
     private float timer;
 
